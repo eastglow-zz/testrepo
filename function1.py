b2d0f8f03e6cd92e58e853d59a3d011d1527c377
HwijaeCho <p0l1mu98@gmail.com>
2022-01-27 14:39:15 +0900

    220127

--- function1.py
+++ function1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.special as sci
 
 xmin = 0
 xmax = 1
@@ -14,16 +15,19 @@ y4 = x*np.log(x) + (1-x)*np.log(1-x)
 y5 = np.exp(-x**2)
 y6 = 1/2 + np.tanh((x-0.5)/0.01)/2
 
+y7 = sci.erf(x)
+
 plt.plot(x,y)
 plt.plot(x,y2)
 plt.plot(x,y3)
 plt.plot(x,y4)
 plt.plot(x,y5)
 plt.plot(x,y6)
+plt.plot(x,y7)
 
 plt.xlabel("x")
 plt.ylabel("y")
 plt.yticks(np.arange(-2,1.1,0.25))
 plt.ylim([-2,1.1])
 
-plt.legend(["y","y2","y3","y4","y5","y6"], loc='lower left')
+plt.legend(["y","y2","y3","y4","y5","y6","y7"], loc='lower left')
